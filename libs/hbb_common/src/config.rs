03ae231318ade1acc01b60de00ceefa35d26f731
SeanJuicer <24528159+SeanJuicer@users.noreply.github.com>
2025-01-09 21:45:09 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.adm1n.top"];
-pub const PUBLIC_RS_PUB_KEY: &str = "5q5a8u28P7UTDEMukOg2uqs9wd0M7UMtoJTwXkmyEwQ=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
